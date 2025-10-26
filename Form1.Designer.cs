c5b8b1a4472519b9bf770a5193e0b7b507423de1
harko <harko@DAEMONSLAPTOP>
2025-10-26 20:12:03 +0200

    Bug fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -91,7 +91,7 @@
             btnCancel.TabIndex = 4;
             btnCancel.Text = "Cancel";
             btnCancel.UseVisualStyleBackColor = true;
-            btnSend.Click += btnCancel_Click;
+            btnCancel.Click += btnCancel_Click;
             // 
             // label2
             // 
